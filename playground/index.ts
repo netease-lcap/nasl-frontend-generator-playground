a8dd6058d3f1716e80b1a33b178394be17927564
吴登轲 <wudengke@corp.netease.com>
2024-03-20 15:47:11 +0800

    feat: 增加文件移动demo

--- playground/index.ts
+++ playground/index.ts
@@ -4,8 +4,11 @@ import {
   MicroFrontendPlugin,
   NASLAppIRBuilderPlugin,
   NASLTranspiler,
+  PlacedFile,
+  ProjectOrganizerPlugin,
   ReactPresetPlugin,
   ServiceMetaKind,
+  Logger,
 } from "@lcap/nasl-unified-frontend-generator";
 import { startDemoTranslation } from "@lcap/nasl-unified-frontend-generator/dist/playground";
 import { Container } from "inversify";
@@ -22,7 +25,22 @@ container
   .bind<MicroFrontendManager>(ServiceMetaKind.MicroFrontendManager)
   .to(MicroFrontendPlugin);
 
-// 启动Demo的翻译
+/**
+ * 修改文件移动方法
+ */
+ProjectOrganizerPlugin.prototype.moveFilesAsYouWant = async function (placedFiles: PlacedFile[]) {
+  const logger = Logger('moveFilesAsYouWant');
+  for (const f of placedFiles) {
+    const originalViewFolderMark = `/__views__/`;
+    if (f.originalAbsolutePath.includes(originalViewFolderMark)) {
+      const to = f.getFolder().replace(originalViewFolderMark, '/views/');
+      logger.info(`move ${f.originalAbsolutePath} from ${f.getFolder()} to ${to}`);
+      f.moveFileTo(to);
+    }
+  }
+}
+
+// 在做完插件修改后，启动Demo的翻译
 startDemoTranslation(container).catch((error) => {
   console.error(error);
 });
