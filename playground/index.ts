28bc5f59f0b8d65ca5028f87f74c8da224d95735
吴登轲 <wudengke@corp.netease.com>
2024-03-20 15:36:55 +0800

    feat: 演示修改依赖注入容器

--- playground/index.ts
+++ playground/index.ts
@@ -1,5 +1,14 @@
+import { IRBuilder, MicroFrontendManager, MicroFrontendPlugin, NASLAppIRBuilderPlugin, NASLTranspiler, ReactPresetPlugin, ServiceMetaKind } from "@lcap/nasl-unified-frontend-generator";
 import { startDemoTranslation } from "@lcap/nasl-unified-frontend-generator/dist/playground";
+import { Container } from 'inversify';
 
-startDemoTranslation().catch((error) => {
+const container = new Container();
+container.bind<NASLTranspiler>(ServiceMetaKind.NASLTranspiler).to(ReactPresetPlugin);
+container.bind<IRBuilder>(ServiceMetaKind.IRBuilder).to(NASLAppIRBuilderPlugin);
+container
+  .bind<MicroFrontendManager>(ServiceMetaKind.MicroFrontendManager)
+  .to(MicroFrontendPlugin);
+
+startDemoTranslation(container).catch((error) => {
   console.error(error);
 });
