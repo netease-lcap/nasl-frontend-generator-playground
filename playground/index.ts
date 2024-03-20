901f44a5bca6cf49f3380f2cef2e4c92358a8ac5
吴登轲 <wudengke@corp.netease.com>
2024-03-20 15:39:35 +0800

    doc: 增加注释

--- playground/index.ts
+++ playground/index.ts
@@ -1,14 +1,28 @@
-import { IRBuilder, MicroFrontendManager, MicroFrontendPlugin, NASLAppIRBuilderPlugin, NASLTranspiler, ReactPresetPlugin, ServiceMetaKind } from "@lcap/nasl-unified-frontend-generator";
+import {
+  IRBuilder,
+  MicroFrontendManager,
+  MicroFrontendPlugin,
+  NASLAppIRBuilderPlugin,
+  NASLTranspiler,
+  ReactPresetPlugin,
+  ServiceMetaKind,
+} from "@lcap/nasl-unified-frontend-generator";
 import { startDemoTranslation } from "@lcap/nasl-unified-frontend-generator/dist/playground";
-import { Container } from 'inversify';
+import { Container } from "inversify";
 
+/**
+ * 默认容器
+ */
 const container = new Container();
-container.bind<NASLTranspiler>(ServiceMetaKind.NASLTranspiler).to(ReactPresetPlugin);
+container
+  .bind<NASLTranspiler>(ServiceMetaKind.NASLTranspiler)
+  .to(ReactPresetPlugin);
 container.bind<IRBuilder>(ServiceMetaKind.IRBuilder).to(NASLAppIRBuilderPlugin);
 container
   .bind<MicroFrontendManager>(ServiceMetaKind.MicroFrontendManager)
   .to(MicroFrontendPlugin);
 
+// 启动Demo的翻译
 startDemoTranslation(container).catch((error) => {
   console.error(error);
 });
