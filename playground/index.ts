131e8471af897310922d1cb303b194f8f4f43942
吴登轲 <wudengke@corp.netease.com>
2024-03-29 19:42:42 +0800

    chore: 修复找不到playground路径的问题

--- playground/index.ts
+++ playground/index.ts
@@ -10,7 +10,7 @@ import {
   NASLDomain,
   JavaScriptDomain,
 } from "@lcap/nasl-unified-frontend-generator";
-import { startDemoTranslation } from "@lcap/nasl-unified-frontend-generator/dist/playground";
+import { startDemoTranslation } from "@lcap/nasl-unified-frontend-generator/playground";
 import { Container } from "inversify";
 
 /**
@@ -20,25 +20,33 @@ const container = new Container();
 container
   .bind<Generator.NASLTranspiler>(ServiceMetaKind.NASLTranspiler)
   .to(ReactPresetPlugin);
-container.bind<NASLDomain.IRBuilder>(ServiceMetaKind.IRBuilder).to(NASLAppIRBuilderPlugin);
 container
-  .bind<JavaScriptDomain.FrontendApplicationDomain.MicroFrontendManager>(ServiceMetaKind.MicroFrontendManager)
+  .bind<NASLDomain.IRBuilder>(ServiceMetaKind.IRBuilder)
+  .to(NASLAppIRBuilderPlugin);
+container
+  .bind<JavaScriptDomain.FrontendApplicationDomain.MicroFrontendManager>(
+    ServiceMetaKind.MicroFrontendManager
+  )
   .to(MicroFrontendPlugin);
 
 /**
  * 修改文件移动方法
  */
-ProjectOrganizerPlugin.prototype.moveFilesAsYouWant = async function (placedFiles: PlacedFile[]) {
-  const logger = Logger('moveFilesAsYouWant');
+ProjectOrganizerPlugin.prototype.moveFilesAsYouWant = async function (
+  placedFiles: PlacedFile[]
+) {
+  const logger = Logger("moveFilesAsYouWant");
   for (const f of placedFiles) {
     const originalViewFolderMark = `/__views__/`;
     if (f.originalAbsolutePath.includes(originalViewFolderMark)) {
-      const to = f.getFolder().replace(originalViewFolderMark, '/views/');
-      logger.info(`move ${f.originalAbsolutePath} from ${f.getFolder()} to ${to}`);
+      const to = f.getFolder().replace(originalViewFolderMark, "/views/");
+      logger.info(
+        `move ${f.originalAbsolutePath} from ${f.getFolder()} to ${to}`
+      );
       f.moveFileTo(to);
     }
   }
-}
+};
 
 // 在做完插件修改后，启动Demo的翻译
 startDemoTranslation(container).catch((error) => {
