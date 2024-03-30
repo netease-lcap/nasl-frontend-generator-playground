dc81f859556bb7cee2c833e2d966eb7f8202ff15
吴登轲 <wudengke@corp.netease.com>
2024-03-30 14:08:23 +0800

    feat: 写入eslint配置

--- playground/index.ts
+++ playground/index.ts
@@ -1,3 +1,4 @@
+import "@abraham/reflection";
 import {
   FileSystemPlugin,
   Generator,
@@ -14,8 +15,9 @@ import {
   ServiceMetaKind,
 } from "@lcap/nasl-unified-frontend-generator";
 import { startDemoTranslation } from "@lcap/nasl-unified-frontend-generator/playground";
-import { Container } from "inversify";
 import dedent from "dedent";
+import { Container, injectable, inject } from "inversify";
+import { merge } from "lodash";
 
 /**
  * 默认容器
@@ -61,27 +63,82 @@ function changeProjectLayout() {
   };
 }
 
+type PackageJSON = Record<string, any>;
+
+const NpmPackageJSONPluginSymbol = Symbol.for("NpmPackageJSONPlugin");
+@injectable()
+class NpmPackageJSONPlugin {
+  constructor(
+    @inject(ServiceMetaKind.FileSystemProvider)
+    private fileSystemProvider: GeneratorInfrastructureDomain.FileSystemProvider
+  ) {}
+
+  private packageJSONPath = "/package.json";
+
+  private readPkg(): PackageJSON | undefined {
+    try {
+      const pkg = this.fileSystemProvider.read(this.packageJSONPath);
+      if (pkg) {
+        return JSON.parse(pkg);
+      }
+    } catch (error) {}
+    return undefined;
+  }
+
+  private writePkg(pkg: PackageJSON) {
+    this.fileSystemProvider.write(this.packageJSONPath, JSON.stringify(pkg));
+  }
+
+  patch(obj: {}) {
+    const pkg = this.readPkg() ?? {};
+    // recursively merge obj to pkg
+    merge(pkg, obj);
+    this.writePkg(pkg);
+  }
+}
+
 function setUpEslint() {
-  const logger = Logger("自定义生命周期插件");
-  container
-    .bind<GeneratorInfrastructureDomain.CodeGenerationLifecycleHooks>(
-      ServiceMetaKind.CodeGenerationLifecycleHooks
-    )
-    .to(
-      class MyLifeCycleHooksPlugin extends LifeCycleHooksPlugin {
-        afterAllFilesGenerated(): void {
-          // 写入eslint相关配置
-          this.fileSystemProvider.write(
-            "/eslint.config.js",
-            dedent`
+  @injectable()
+  class MyEslintPlugin extends LifeCycleHooksPlugin {
+    constructor(
+      @inject(ServiceMetaKind.FileSystemProvider)
+      protected fileSystemProvider: GeneratorInfrastructureDomain.FileSystemProvider,
+      @inject(NpmPackageJSONPluginSymbol)
+      private npmPackageJSONPlugin: NpmPackageJSONPlugin
+    ) {
+      super(fileSystemProvider);
+    }
+    afterAllFilesGenerated(): void {
+      const logger = Logger("自定义生命周期插件");
+      // 写入eslint相关配置
+      this.fileSystemProvider.write(
+        "/eslint.config.js",
+        dedent`
           module.exports  = {
             "extends": ["eslint:recommended", "plugin:react/recommended"]
           }
           `
-          );
-        }
-      }
-    );
+      );
+      this.npmPackageJSONPlugin.patch({
+        devDependencies: {
+          eslint: "^7.32.0",
+          "eslint-plugin-react": "^7.26.1",
+        },
+      });
+      const res = this.fileSystemProvider.read("/package.json");
+      logger.info(res, "package.json内容");
+    }
+  }
+
+  container
+    .bind<NpmPackageJSONPlugin>(NpmPackageJSONPluginSymbol)
+    .to(NpmPackageJSONPlugin);
+
+  container
+    .bind<GeneratorInfrastructureDomain.CodeGenerationLifecycleHooks>(
+      ServiceMetaKind.CodeGenerationLifecycleHooks
+    )
+    .to(MyEslintPlugin);
 }
 
 changeProjectLayout();
