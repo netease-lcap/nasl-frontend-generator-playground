7875e6ed18f29f046a1317ed5b00c21a7b5addfa
吴登轲 <wudengke@corp.netease.com>
2024-05-13 10:44:58 +0800

    feat: 实现端入口分派

---
+++ playground/compile.ts
@@ -0,0 +1,40 @@
+import { App, Frontend } from "@lcap/nasl";
+
+async function compile() {
+  // read NASL from /workspace/src.json
+  const app = new App();
+  // read FRONTENDS from env
+  const FRONTENDS = [
+    {
+      name: "pc",
+      type: "pc",
+      path: "/",
+      selected: true,
+      title: "PC端",
+    },
+    {
+      name: "m",
+      type: "h5",
+      path: "/m",
+      selected: true,
+      title: "H5端",
+    },
+  ];
+
+  for (const frontend of FRONTENDS) {
+    const path = `app.frontendTypes.${frontend.type}.${frontend.name}`;
+    const f = app.findNodeByCompleteName(path) as Frontend | undefined;
+    if (f) {
+      const kind = f.frameworkKind;
+      if (kind === "vue2") {
+        // TODO vue2
+      } else {
+        // TODO react
+      }
+    } else {
+      throw new Error(`no such frontend: ${path}`);
+    }
+  }
+}
+
+export default compile;
