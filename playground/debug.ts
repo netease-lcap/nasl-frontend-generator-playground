b227d12b8c8d555fb7366d64cda78a89ab0701e5
吴登轲 <wudengke@corp.netease.com>
2024-05-17 13:21:57 +0800

    fix: 增加写入文件的输出

--- playground/debug.ts
+++ playground/debug.ts
@@ -1076,6 +1076,10 @@ logger.info("开始翻译");
 
 // @ts-expect-error TODO config 类型有问题
 translate(config).then(async (files) => {
+  logger.info(
+    files.map((x) => x.path),
+    "写入文件"
+  );
   // pick name from data
   await writeCode(files);
 });
