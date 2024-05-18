efa8deeb41008fc3fbf05cdf32a77a82e362f402
吴登轲 <wudengke@corp.netease.com>
2024-05-18 15:20:25 +0800

    refactor: 重命名逻辑

--- playground/translate.ts
+++ playground/translate.ts
@@ -39,8 +39,8 @@ export async function translate(
           frontendNode,
           config as any
         );
-        function normalizeFileNames(files: NameContent[]) {
-          const normalizedFiles = files.map(({ name, content }) => {
+        function transformFileNameToObjectKey(files: NameContent[]) {
+          const filesInObjectKey = files.map(({ name, content }) => {
             // name 的例子 "//minio-api.codewave-test.163yun.com/lowcode-static/defaulttenant/02195780-b1da-450b-a95e-d93147f02d7c/dev/22042518.min.js"
             const startingMarks = `${config.STATIC_URL}/${config.tenant}/${app.id}/${config.env}`;
             const normalizedName = lightJoin(
@@ -52,10 +52,10 @@ export async function translate(
               content,
             };
           });
-          return normalizedFiles;
+          return filesInObjectKey;
         }
-        const normalized = normalizeFileNames(files);
-        res.push(...normalized);
+        const filesInObjectKey = transformFileNameToObjectKey(files);
+        res.push(...filesInObjectKey);
       } else if (kind === "react") {
         // TODO 实现 react
         throw new Error("TODO: react");
