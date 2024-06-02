489c1602507f4a5ca5e53d4704fd74e9d4b80afe
吴登轲 <wudengke@corp.netease.com>
2024-06-02 20:09:29 +0800

    feat: 提供前端翻译器脚手架

---
+++ scripts/build.ts
@@ -0,0 +1,74 @@
+import { confirm, input } from "@inquirer/prompts";
+import fs from "fs-extra";
+import path from "path";
+import inc from "semver/functions/inc";
+import { v4 as uuidV4 } from "uuid";
+import { build } from 'tsup'
+
+const descriptionFilePath = path.join(__dirname, "../public/description.json");
+
+type TranslatorPluginDescription = {
+  symbol: string;
+  name: string;
+  version: string;
+  ideVersion: string;
+  description: string;
+  endType: string;
+};
+
+async function main() {
+  const exists = fs.existsSync(descriptionFilePath);
+  console.log("开始打包插件");
+  if (!exists) {
+    const descObj: TranslatorPluginDescription = {
+      name: await input({
+        message: "请输入插件名",
+        default: "我的翻译器插件",
+      }),
+      version: await input({
+        message: "请输入插件版本号",
+        default: "1.0.0",
+      }),
+      ideVersion: "3.8",
+      description: "测试",
+      endType: "frontend",
+      symbol: uuidV4(),
+    };
+    fs.writeJsonSync(descriptionFilePath, descObj, { spaces: 2 });
+  } else {
+    const desc: TranslatorPluginDescription =
+      fs.readJsonSync(descriptionFilePath);
+    const version = desc.version;
+    const res = await confirm({
+      message: `是否更新[${desc.name}@${desc.version}]的版本号`,
+    });
+    if (res) {
+      const newVersion = await input({
+        message: `请输入新的版本号`,
+        default: inc(version, "patch") ?? version,
+      });
+      desc.version = newVersion;
+    }
+    fs.writeJSONSync(descriptionFilePath, desc, { spaces: 2 });
+  }
+  const desc: TranslatorPluginDescription =
+    fs.readJsonSync(descriptionFilePath);
+  console.log(desc);
+  const res = await confirm({
+    message: `是否进行打包`,
+  });
+  if (res) {
+    await build({});
+    pack();
+    console.log("打包完成");
+  }
+}
+
+main();
+
+function pack() {
+  const zip = require("cross-zip");
+  const inPath = path.join(__dirname, "../dist");
+  const outPath = path.join(__dirname, `../plugin.zip`);
+  zip.zipSync(inPath, outPath);
+}
