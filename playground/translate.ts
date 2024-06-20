b0422a81ff7aadf5669b47a8e5248ff87eb9eac8
吴登轲 <wudengke@corp.netease.com>
2024-06-20 17:46:11 +0800

    fix: 更新为了获取packageInfos的版本号

--- playground/translate.ts
+++ playground/translate.ts
@@ -15,7 +15,7 @@ async function init(app: App, config: CommonAppConfig) {
   await tempUtils.getAndLoadPackageInfos(app, {
     staticUrl: config.STATIC_URL,
     // TODO wudengke 拿到完全的版本号
-    fullVersion: "3.8.0",
+    fullVersion: "3.9.0",
   });
 }
 
