f93f5820e09b19f07ee37b1585fc599b883f72c1
吴登轲 <wudengke@corp.netease.com>
2024-10-17 14:13:29 +0800

    chore: 脚手架提供的makeContainer改为默认行为

--- playground/container.ts
+++ playground/container.ts
@@ -1,14 +1,20 @@
 import { makeDefaultContainer } from "@lcap/nasl-unified-frontend-generator";
-import { setupPerformanceOptions } from "./customization/custom-performance";
-import { setupNpmPackages } from "./customization/custom-npm-package";
-import { setupCompilerToWebpack } from "./customization/custom-compiler";
-import { setupMicrofrontend } from "./customization/custom-microfrontend";
+
+// 样例：修改默认容器中的内容。替换makeContainer为如下代码
+// import { setupPerformanceOptions } from "./customization/custom-performance";
+// import { setupNpmPackages } from "./customization/custom-npm-package";
+// import { setupCompilerToWebpack } from "./customization/custom-compiler";
+// import { setupMicrofrontend } from "./customization/custom-microfrontend";
+// export async function makeContainer() {
+//   const container = makeDefaultContainer(); // 构造默认容器
+//   return Promise.resolve(container)
+//     .then(setupPerformanceOptions) // 修改性能配置
+//     .then(setupNpmPackages) // 修改npm包
+//     .then(setupCompilerToWebpack) // 修改前端编译器为Webpack
+//     .then(setupMicrofrontend); // 修改微前端配置
+// }
 
 export async function makeContainer() {
   const container = makeDefaultContainer(); // 构造默认容器
-  return Promise.resolve(container)
-    .then(setupPerformanceOptions) // 修改性能配置
-    .then(setupNpmPackages) // 修改npm包
-    .then(setupCompilerToWebpack) // 修改前端编译器为Webpack
-    .then(setupMicrofrontend); // 修改微前端配置
+  return Promise.resolve(container);
 }
